87e66c03edc338b2d9a444a6ecea0feaba8efecb
Petrux25 <diesveca@hotmail.com>
2017-10-05 16:46:01 -0600

    Player class creation
    name,ip, arraylist created

---
+++ UnoServer/src/player/Player.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package player;
+
+/**
+ *
+ * @author diesv
+ */
+public class Player {
+   String name;
+   String ip;
+   
+   public Player(String name, String ip ){
+       this.name=name;
+       this.ip=ip;
+       
+   }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+    
+}
